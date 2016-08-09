8c7300495d4695478d4eed773de2c35e643ca6d5
Clark Chen <chenhuajie1@gmail.com>
2016-08-09 01:20:19 -0400

    finish exercise2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#10% to 15% tips
+puts (55 * rand(1.10..1.15)).round(2)
+
+#string integer concatenation
+puts 5.to_s + " bucks"
+
+#integer calculation within string
+puts "#{45628 * 7839}"
+
+#expecting "true"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
